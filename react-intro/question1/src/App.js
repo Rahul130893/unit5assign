feac8277cbaa854717f77bcbd82a4929a1dc493d
Rahul130893 <93374760+Rahul130893@users.noreply.github.com>
2022-04-10 01:02:43 +0530

    done

--- react-intro/question1/src/App.js
+++ react-intro/question1/src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     
+      <h1> Mobile Operating system</h1>
+      <div className="firstDiv">
+        <ul className="list">
+          <li>Android </li>
+          <li>Blackberry</li>
+          <li>iPhone</li>
+          <li>Windows Phone</li>
+        </ul>
+      </div>
+      <h1> Mobile Manufacturers</h1>
+      <div className="firstDiv">
+        <ul className="list">
+          <li>Samsung</li>
+          <li>HTC</li>
+          <li>Micromax</li>
+          <li>Apple</li>
+        </ul>
+      </div>
     </div>
   );
 }
